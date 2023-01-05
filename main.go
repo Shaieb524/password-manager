bd3aec785d05e1afb77097cf4bf82584dc9fa418
Joud Shaieb <joud@cxunicorn.com>
2023-01-05 20:01:35 +0400

    comment wire stuff

--- main.go
+++ main.go
@@ -2,22 +2,25 @@ package main
 
 import (
 	"fmt"
-	// apController "password-manager/src/controllers/accountPassword"
-	// apRepo "password-manager/src/models/database/accountPassword"
-	// "password-manager/src/providers/database"
+	apController "password-manager/src/controllers/accountPassword"
+	apRepo "password-manager/src/models/database/accountPassword"
+	"password-manager/src/providers/database"
 	"password-manager/src/routes"
-	wire "password-manager/src/wire"
-	// apService "password-manager/src/services/accountPassword"
+
+	// web "password-manager/src/web"
+	apService "password-manager/src/services/accountPassword"
 )
 
 func main() {
-	// db := database.NewDatabaseContext()
-	// repo := apRepo.NewAccPasswordRepoModule(db)
-	// service := apService.NewAccPasswordServiceModule(repo)
-	// apCC := apController.NewAccPasswordControllerModule(service)
+
 	fmt.Println("Start bitch")
 	routes.LoadEnv()
 	routes.LoadDatabase()
-	api := wire.InitAccountPasswordAPI()
-	routes.SetupRoutesAndRun(api)
+	db := database.NewDatabaseContext()
+	repo := apRepo.NewAccPasswordRepoModule(db)
+	service := apService.NewAccPasswordServiceModule(repo)
+	apCC := apController.NewAccPasswordControllerModule(service)
+
+	// api := web.InitAccountPasswordAPI()
+	routes.SetupRoutesAndRun(apCC)
 }
