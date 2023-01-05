6715b574bb45b81b84cc09f58dea67e5870aa683
Joud Shaieb <joud@cxunicorn.com>
2023-01-05 20:01:08 +0400

    comment wire stuff

---
+++ src/web/wire.go
@@ -0,0 +1,16 @@
+package web
+
+import (
+	apController "password-manager/src/controllers/accountPassword"
+	apRepo "password-manager/src/models/database/accountPassword"
+	dB "password-manager/src/providers/database"
+	apService "password-manager/src/services/accountPassword"
+
+	"github.com/google/wire"
+)
+
+func InitAccountPasswordAPI() *apController.AccountPasswordController {
+	wire.Build(dB.NewDatabaseContext, apRepo.NewAccPasswordRepoModule, apService.NewAccPasswordServiceModule, apController.NewAccPasswordControllerModule)
+
+	return &apController.AccountPasswordController{}
+}
